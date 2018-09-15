44c673766208469d6d787cadfcf4f136fec05450
klisiewiczm@gmail.com <Tygrys15111985>
2018-09-15 13:28:42 +0200

    Zostaly wykonane testy tylko dla 1 uzytkownika. Multiply user tbd.,,

--- src/test/Resources/example.feature
+++ src/test/Resources/example.feature
@@ -1,5 +1,19 @@
+
+
 Feature: Open Website
+
+
   Scenario: Open Firefox
     Given type codersguru
     Then Goto add new user
     When type in to formular
+    And  type username
+    And  type Last Name
+    And  Password
+    And  Repeat password
+    And  Type City Name
+    And  Type Zip-Code
+    And  Type Street Name
+    And  Type Home Number
+    And  Click "agree"
+    And  Click register
--- src/test/java/ExampleSteps/ExampleSteps.java
+++ src/test/java/ExampleSteps/ExampleSteps.java
@@ -1,9 +1,11 @@
 package ExampleSteps;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -22,14 +24,72 @@ public class ExampleSteps {
 
     @Then("^Goto add new user$" )
     public void goto_add_new_user() throws Throwable {
-        System.out.println("Wpisuje uzytkowanikow.");
+        driver.findElement(By.xpath("/html/body/section[3]/div/form/input[2]")).click();
 
     }
 
     @When("^type in to formular$" )
     public void type_in_to_formular() throws Throwable {
-        System.out.println("Wypelniam formularz.");
+        driver.findElement(By.xpath("//*[@id=\"fos_user_registration_form_email\"]")).sendKeys("username12@porkds.pl");
+
+    }
+    @And("^type username$")
+    public void type_username() throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"fos_user_registration_form_name\"]")).sendKeys("username12");
 
     }
 
+    @And("^type Last Name$")
+    public void type_Last_Name() throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"fos_user_registration_form_lastname\"]")).sendKeys("dasda");
+
+    }
+
+    @When("^Password$")
+    public void password() throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"fos_user_registration_form_plainPassword_first\"]")).sendKeys("username12");
+
+    }
+
+    @When("^Repeat password$")
+    public void repeat_password() throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"fos_user_registration_form_plainPassword_second\"]")).sendKeys("username12");
+        //throw new PendingException();
+    }
+
+    @When("^Type City Name$")
+    public void type_City_Name() throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"form_city\"]")).sendKeys("username12");
+        //throw new PendingException();
+    }
+
+    @When("^Type Zip-Code$")
+    public void type_Zip_Code() throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"form_postal_code\"]")).sendKeys("54-100");
+        //throw new PendingException();
+    }
+
+    @When("^Type Street Name$")
+    public void type_Street_Name() throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"form_street\"]")).sendKeys("username12");
+        //throw new PendingException();
+    }
+
+    @When("^Type Home Number$")
+    public void type_Home_Number() throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"form_number\"]")).sendKeys("username12");
+        //throw new PendingException();
+    }
+
+    @When("^Click \"([^\"]*)\"$")
+    public void click(String arg1) throws Throwable {
+        driver.findElement(By.xpath("/html/body/div/div/div/form/div[12]/input ")).click();
+        //throw new PendingException();
+    }
+
+    @When("^Click register$")
+    public void click_register() throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"register-submit-btn\"]")).click();
+        //throw new PendingException();
+    }
 }
